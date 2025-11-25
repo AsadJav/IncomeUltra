d27d8d74460a40c27303d175e86c183f5a30955f
AsadJav <asadjaved9490@gmail.com>
2025-11-26 01:12:16 +0500

    Initial Commit

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by user on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
